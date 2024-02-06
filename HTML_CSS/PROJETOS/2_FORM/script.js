32d75d6c6bec3b6c647672b143b66cd3de970d5f
Usuario <gustavoalbonicogoncalves@hotmail.com>
2024-02-05 23:39:10 -0300

    finalizado projeto form

--- HTML_CSS/PROJETOS/2_FORM/script.js
+++ HTML_CSS/PROJETOS/2_FORM/script.js
@@ -1,7 +1,9 @@
 mudaValor()
 
     function mudaValor() {
-        minHigh.innerText = idMinHigh.value;
-        maxHigh.innerText = idMaxHigh.value;
-        maxDistance.innerText = idMaxDistance.value;
+        minHigh.innerText = idMinHigh.value + 'm';
+        maxHigh.innerText = idMaxHigh.value + 'm';
+        maxDistance.innerText = idMaxDistance.value + 'km';
+        maxAge.innerText = idMaxAge.value + ' anos';
+        minAge.innerText = idMinAge.value + ' anos';
     }
