c5d88e2b2647215c54ab718d58c1224cf7508458
GustavoAlbonico <gustavoalbonicogoncalves@hotmail.com>
2024-03-05 19:19:06 -0300

    Iniciando javascript

---
+++ JAVASCRIPT/2_OPERADORES/js/script.js
@@ -0,0 +1,35 @@
+// 1- Number
+console.log(typeof 2);
+console.log(typeof 5.14);
+console.log(typeof -127);
+
+// 2 - Ops. aritiméticas
+console.log(2 + 4);
+console.log(10 - 5);
+console.log(20 / 5);
+console.log(200 / 56);
+console.log(5 * 8);
+console.log(14 * 9);
+console.log(5 + (4 * 2));
+
+//  3 - Special numbers
+console.log(typeof Infinity);
+
+console.log(typeof -Infinity);
+
+console.log(12 * "asa");
+
+console.log(typeof NaN);
+
+// 4 -Strings
+console.log("Um texto");
+console.log('Mais um texto');
+console.log('13');
+
+console.log(typeof "Um texto");
+console.log(typeof 'Mais um texto');
+
+// 5 - Símbolos especiais em string
+console.log("Testando a\nquebra de linha");
+
+console.log("Espaçamento \t de tab");
