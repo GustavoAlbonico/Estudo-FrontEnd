a121483cd745e373a7e95eac311773e393738ad4
GustavoAlbonico <gustavoalbonicogoncalves@hotmail.com>
2024-03-06 19:11:48 -0300

    Finalizando operadores

--- JAVASCRIPT/2_OPERADORES/js/script.js
+++ JAVASCRIPT/2_OPERADORES/js/script.js
@@ -31,5 +31,59 @@ console.log(typeof 'Mais um texto');
 
 // 5 - Símbolos especiais em string
 console.log("Testando a\nquebra de linha");
-
 console.log("Espaçamento \t de tab");
+
+// 6 -Concatenação
+console.log("Oi," + " tudo" + " bem?");
+console.log(`Testando ` + `com ` + `crase!`);
+
+// 7 - Template Strings
+console.log(`A soma de 2 + 2 é: ${2 + 2}`);
+console.log(`Podemos executar qualquer coisa aqui ${console.log("teste")}`)
+
+// 8 - Booleans
+console.log(true);
+console.log(5 > 20);
+console.log(30 > 10);
+console.log(typeof false);
+
+// 9 - Comparações
+console.log(5 <= 5);
+console.log(5 <= 5);
+console.log(10 == 10);
+console.log(10 == 9);
+console.log(10 != 9);
+
+// 10 - Idêntico
+console.log(9 == "9");
+console.log(9 === "9");
+
+console.log(9 != "9");
+console.log(9 !== "9");
+
+// 11 - Operadores lógicos
+console.log(true && true);
+console.log(true && false);
+
+console.log(5 > 2 && 2 < 10);
+console.log(5 > 2 && "Matheus" === 1);
+console.log(5 > 2 || "Matheus" === 1);
+console.log(5 > 2 || 5 > 100);
+console.log(!true);
+console.log(!5 > 2);
+
+// 12 - empty values
+console.log(typeof null, typeof undefined);
+
+console.log(null == undefined);
+
+console.log(null === undefined);
+
+// 13 - mudança de tipo
+console.log(5 * null);
+console.log("oi" * "opa");
+console.log("10" + 1);
+console.log("10" - 1);
+
+
+
---
+++ JAVASCRIPT/3_ESTRUTURAS/js/script.js
@@ -0,0 +1,2 @@
+// 1 -  variaveis
+let 
