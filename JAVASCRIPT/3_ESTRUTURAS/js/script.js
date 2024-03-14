e3c0cbef7c0420cf8f65fcadbe861404138acbfe
GustavoAlbonico <gustavoalbonicogoncalves@hotmail.com>
2024-03-14 19:09:15 -0300

    foreach...

--- JAVASCRIPT/3_ESTRUTURAS/js/script.js
+++ JAVASCRIPT/3_ESTRUTURAS/js/script.js
@@ -1,103 +1,103 @@
-// 1 -  variaveis
-// let nome = "Matheus";
+1 -  variaveis
+let nome = "Matheus";
 
-// console.log(nome);
-// console.log("Matheus");
+console.log(nome);
+console.log("Matheus");
 
-// nome = "Matheus Da Silva";
+nome = "Matheus Da Silva";
 
-// console.log(nome);
+console.log(nome);
 
-// const idade = 18;
+const idade = 18;
 
-// console.log(idade);
+console.log(idade);
 
-// console.log(typeof nome);
-// console.log(typeof idade);
+console.log(typeof nome);
+console.log(typeof idade);
 
-// // 2 - mais sobre variaveis
+// 2 - mais sobre variaveis
 
-// let a = 10 , b = 20 , c = 30;
+let a = 10 , b = 20 , c = 30;
 
-// console.log(a,b,c);
+console.log(a,b,c);
 
-// const nomeCompleto = "Matheus Batisti";
+const nomeCompleto = "Matheus Batisti";
 
-// const nomecompleto = "Joao da silva";
+const nomecompleto = "Joao da silva";
 
-// console.log(nomeCompleto);
-// console.log(nomecompleto);
+console.log(nomeCompleto);
+console.log(nomecompleto);
 
-// // 3 - prompt
-// // const age = prompt("DIgite a sua idade:");
+// 3 - prompt
+const age = prompt("DIgite a sua idade:");
 
-// // console.log(`Você tem ${age} anos.`);
+console.log(`Você tem ${age} anos.`);
 
-// // 4 - alert
-// // alert("Testando");
+// 4 - alert
+alert("Testando");
 
-// // const z = 10;
+const z = 10;
 
-// // alert(`O numero é ${z}`);
+alert(`O numero é ${z}`);
 
-// // 5 - Math
-// // console.log(Math.max(5,2,1,10));
-// // console.log(Math.floor(5.14));
-// // console.log(Math.ceil(5.14));
+// 5 - Math
+console.log(Math.max(5,2,1,10));
+console.log(Math.floor(5.14));
+console.log(Math.ceil(5.14));
 
-// // // 6 - console
-// // console.log("Teste!");
-// // console.error("error!");
-// // console.warn("aviso!");
+// 6 - console
+console.log("Teste!");
+console.error("error!");
+console.warn("aviso!");
 
-// // 7 - if
-// const m = 10;
+// 7 - if
+const m = 10;
 
-// if(m > 5){
-//     console.log("M é maior que 5!");
-// }
+if(m > 5){
+    console.log("M é maior que 5!");
+}
 
-// const user = "João";
+const user = "João";
 
-// if(user === "João"){
-//     console.log("Olá João");
-// }
+if(user === "João"){
+    console.log("Olá João");
+}
 
-// // 8 - else
-// const loggedIn =  false;
+// 8 - else
+const loggedIn =  false;
 
-// if(loggedIn){
-//     console.log("Esta autenticado!");
-// } else {
-//     console.log("Não está autenticado");
-// }
+if(loggedIn){
+    console.log("Esta autenticado!");
+} else {
+    console.log("Não está autenticado");
+}
 
-// // 9 - else if
+// 9 - else if
 
-// if(1 > 2){
-//     console.log("Teste");
-// } else if (2 > 3){
-//     console.log("Teste 2");
-// } else if (5 > 1){
-//     console.log("Agora sim!");
-// }
+if(1 > 2){
+    console.log("Teste");
+} else if (2 > 3){
+    console.log("Teste 2");
+} else if (5 > 1){
+    console.log("Agora sim!");
+}
 
-// 10 - While
+10 - While
 
-// let p = 0
+let p = 0
 
-// while(p < 5){
-//     console.log(`Repetindo ${p}`);
-//     p = p + 1;
-// }
+while(p < 5){
+    console.log(`Repetindo ${p}`);
+    p = p + 1;
+}
 
-// // 11 - Do While
-// let o = 10;
+// 11 - Do While
+let o = 10;
 
-// do {
-//     console.log(`Valor de o:${o}`)
-//     o--
-// } while (o > 1);
+do {
+    console.log(`Valor de o:${o}`)
+    o--
+} while (o > 1);
 
 // 12 - for
 for(let t = 0; t < 10; t++){
--- JAVASCRIPT/5_ARRAYS_OBJETOS/js/script.js
+++ JAVASCRIPT/5_ARRAYS_OBJETOS/js/script.js
@@ -126,58 +126,141 @@
 //     console.log(`Listando o usuário: ${users[i]}`);
 // }
 
-// 11 - push e pop
-const array = ["a","b","c"];
+// // 11 - push e pop
+// const array = ["a","b","c"];
 
-array.push("d");
+// array.push("d");
 
-console.log(array);
+// console.log(array);
 
-console.log(array.length);
+// console.log(array.length);
 
-array.pop();
+// array.pop();
 
-console.log(array);
+// console.log(array);
 
-const itemRemovido = array.pop();
+// const itemRemovido = array.pop();
 
-console.log(itemRemovido);
+// console.log(itemRemovido);
 
-console.log(array);
+// console.log(array);
 
-array.push("z","x","y");
+// array.push("z","x","y");
 
-console.log(array);
+// console.log(array);
 
-// 12 - shift e unshift
+// // 12 - shift e unshift
 
-const letters = ["a","b","c"];
+// const letters = ["a","b","c"];
 
-const letter = letters.shift();
+// const letter = letters.shift();
 
-console.log(letter);
+// console.log(letter);
 
-console.log(letters);
+// console.log(letters);
 
-letters.unshift("p","q","r");
+// letters.unshift("p","q","r");
 
-letters.unshift("z");
+// letters.unshift("z");
 
-console.log(letters);
+// console.log(letters);
 
-// 13 - indexOf e lastIndexOf
-const myElements = ["Morango","Maçã","Abacate","Pêra","Abacate"];
+// // 13 - indexOf e lastIndexOf
+// const myElements = ["Morango","Maçã","Abacate","Pêra","Abacate"];
 
-console.log(myElements.indexOf("Maçã"));
-console.log(myElements.indexOf("Abacate"));
+// console.log(myElements.indexOf("Maçã"));
+// console.log(myElements.indexOf("Abacate"));
 
-console.log(myElements[2]);
-console.log(myElements[myElements.indexOf("Abacate")]);
+// console.log(myElements[2]);
+// console.log(myElements[myElements.indexOf("Abacate")]);
 
-console.log(myElements.lastIndexOf("Abacate"));
+// console.log(myElements.lastIndexOf("Abacate"));
 
-console.log(myElements.indexOf("Mamão"));
+// console.log(myElements.indexOf("Mamão"));
 
-console.log(myElements.lastIndexOf("Mamão"));
+// console.log(myElements.lastIndexOf("Mamão"));
 
+// // 14 -  slice
+// const testeSlice = ["a","b","c","d","e","f"];
 
+// const subArray =  testeSlice.slice(2,4);
+// //ele pega do index 2 até o index 3 na verdade
+
+// console.log(subArray);
+
+// console.log(testeSlice);
+
+// const subArray2 = testeSlice.slice(2, 4 + 1);
+
+// console.log(subArray2);
+
+// const subArray3 =  testeSlice.slice(10,20);
+
+// console.log(subArray3);
+
+// const subArray4 = testeSlice.slice(2);
+
+// console.log(subArray4);
+
+// const arrayTeste = ["00:00","00:30","01:00","01:30","02:00"];
+
+// console.log(arrayTeste.slice(2));
+
+// // 15 - foreach
+// const nums = [1, 2, 3, 4, 5];
+
+// nums.forEach((numero) => {
+//     console.log(`O número é ${numero}`);
+// });
+
+// const post = [
+//     { title: "Primeiro post", category: "PHP"},
+//     { title: "Segundo post", category: "JavaScript"},
+//     { title: "Terceito post", category: "Python"},
+// ];
+
+// post.forEach((post) => {
+//     console.log(`Exibindo post: ${post.title}, da categoria: ${post.category}`);
+// });
+
+// // 16 - includes
+// const brands = ["BMW", "VW", "Fiat"];
+
+// console.log(brands.includes("Fiat"));
+
+// console.log(brands.includes("KIA"));
+
+// if (brands.includes("BMW")) {
+//     console.log("Há carros da marca BMW!");
+// }
+
+// // 17 - reverse
+// const reverseTest = [1, 2, 3, 4, 5];
+
+// reverseTest.reverse();
+
+// console.log(reverseTest);
+
+// 18 - trim (remove espaço e caracteres especiais tipo /n)
+const trimTest = "   testando \n  ";
+
+console.log(trimTest);
+
+console.log(trimTest.trim());
+
+console.log(trimTest.length);
+
+console.log(trimTest.trim().length);
+
+// 19 - padstart (adiciona texto no começo da string)
+const testePadStart = "1";
+
+const newNumber =  testePadStart.padStart(4 , "0");
+
+console.log(testePadStart);
+
+console.log(newNumber);
+
+const testePadEnd =  newNumber.padEnd(10, "0");
+
+console.log(testePadEnd);
