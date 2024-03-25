416389062634bc18e2963b5a261c629a92c2d17e
GustavoAlbonico <gustavoalbonicogoncalves@hotmail.com>
2024-03-25 19:13:41 -0300

    Iniciando seção DEBUG

---
+++ JAVASCRIPT/7_DEBUG/js/script.js
@@ -0,0 +1,30 @@
+// 1 - strict
+"use strict";
+
+// opa = "teste";
+
+const opa = "teste";
+
+// const undefined = 10;
+
+// delete [].lenght;
+
+// 2 - debug
+let a = 1;
+let b = 2;
+
+if (a == 1){
+    a = b + 2;
+}
+
+console.log(a);
+
+for(let i = 0; i < b; i++){
+    a = a + 2;
+    console.log(a);
+}
+
+if(a > 5){
+ a = 25;
+}
+console.log(a);
