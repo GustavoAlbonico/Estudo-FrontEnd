3c10193999528f53241e0db258f83770843a3a61
GustavoAlbonico <gustavoalbonicogoncalves@hotmail.com>
2024-04-03 18:56:36 -0300

    Iniciando seção sobre DOM

--- JAVASCRIPT/8_ASYNC/js/script.js
+++ JAVASCRIPT/8_ASYNC/js/script.js
@@ -1,44 +1,44 @@
-// // 1 - setTimeout
-// console.log("Ainda não executou");
+// 1 - setTimeout
+console.log("Ainda não executou");
 
-// setTimeout(function() {
-//     console.log("Requisição assíncrona");
-// }, 2000);
+setTimeout(function() {
+    console.log("Requisição assíncrona");
+}, 2000);
 
-// console.log("Ainda não executou 2");
+console.log("Ainda não executou 2");
 
-// // 2 - setInterval
-// console.log("Ainda não executou 2");
+// 2 - setInterval
+console.log("Ainda não executou 2");
 
-// // setInterval(function (){
-// //     console.log("Intervalo assíncrono");
-// // }, 3000);
+// setInterval(function (){
+//     console.log("Intervalo assíncrono");
+// }, 3000);
 
-// console.log("Ainda não começou 2");
+console.log("Ainda não começou 2");
 
-// // 3 - Promise (executado apos todo o codigo assincrono)
-// const promessa =  Promise.resolve(5 + 5);
+// 3 - Promise (executado apos todo o codigo assincrono)
+const promessa =  Promise.resolve(5 + 5);
 
-// console.log("Algum código");
+console.log("Algum código");
 
-// promessa
-//     .then((value) => {
-//         console.log(`A soma é ${value}`);
-//         return value;
-//     })
-//     .then((value) => value - 1)
-//     .then((value) => console.log(`Agora é ${value}`));
+promessa
+    .then((value) => {
+        console.log(`A soma é ${value}`);
+        return value;
+    })
+    .then((value) => value - 1)
+    .then((value) => console.log(`Agora é ${value}`));
 
-//     console.log("Outro código");
+    console.log("Outro código");
 
-// // 4 - falha na promise
-// Promise.resolve(4 * "asd")
-// .then((n) => {
-//     if(Number.isNaN(n)){
-//         throw new Error("Valores inválidos");
-//     }
-// })
-// .catch((err) => console.log(`Um erro ocorreu: ${err}`));
+// 4 - falha na promise
+Promise.resolve(4 * "asd")
+.then((n) => {
+    if(Number.isNaN(n)){
+        throw new Error("Valores inválidos");
+    }
+})
+.catch((err) => console.log(`Um erro ocorreu: ${err}`));
 
 // 5 - rejeição
 function checkNumber(n) {
