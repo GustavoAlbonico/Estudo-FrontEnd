05fda523c9b0808cc2e661022088c138013bbc3b
GustavoAlbonico <gustavoalbonicogoncalves@hotmail.com>
2024-04-04 19:01:55 -0300

    manipulando o DOM

--- JAVASCRIPT/9_DOM/js/script.js
+++ JAVASCRIPT/9_DOM/js/script.js
@@ -11,3 +11,36 @@ console.log(document.body.childNodes[1].childNodes[1].textContent);
 const listItens =  document.getElementsByTagName("li");
 
 console.log(listItens);
+
+// 3 - selecionando elemento por id
+const title = document.getElementById("title");
+
+console.log(title);
+
+// 4 - selecionado elementos por classe
+const products = document.getElementsByClassName("product");
+
+console.log(products);
+
+// 5 - selecionando os elementos por CSS
+const productsQuery =  document.querySelectorAll(".product");
+
+console.log(productsQuery);
+
+const mainContainer = document.querySelector("#main-container");
+
+console.log(mainContainer);
+
+// 6 -insertBefore
+const p = document.createElement("p");
+
+const header = title.parentElement;
+
+header.insertBefore(p, title);
+
+// 7 - appendChild
+const navLinks = document.querySelector("nav ul");
+
+const li = document.createElement("li");
+
+navLinks.appendChild(li);
