763790e0b8e4c63b59f2d906f309bd0144d85ee7
GustavoAlbonico <gustavoalbonicogoncalves@hotmail.com>
2024-04-08 19:02:33 -0300

    Finalizando estudo do DOM

--- JAVASCRIPT/9_DOM/js/script.js
+++ JAVASCRIPT/9_DOM/js/script.js
@@ -44,3 +44,58 @@ const navLinks = document.querySelector("nav ul");
 const li = document.createElement("li");
 
 navLinks.appendChild(li);
+
+// 8 - replaceChild
+const h2 = document.createElement("h2");
+
+h2.textContent = "Meu novo título";
+
+header.replaceChild(h2,title);
+
+// 9 - createTextNode
+const myText = document.createTextNode("Agora vamos colocar mais um título");
+
+console.log(myText);
+
+const h3 = document.createElement("h3");
+
+h3.appendChild(myText);
+
+console.log(h3);
+
+mainContainer.appendChild(h3);
+
+// 10 - trabalhando com atributos
+const firstLink =  navLinks.querySelector("a");
+
+console.log(firstLink);
+
+firstLink.setAttribute("href","https://google.com");
+
+console.log(firstLink.getAttribute("href"));
+
+firstLink.setAttribute("target","_blank");
+
+// 11 - altura e largura
+const  footer =  document.querySelector("footer");
+
+console.log(footer.offsetWidth);
+console.log(footer.offsetHeight);
+
+console.log(footer.clientWidth); //SEM BORDA
+console.log(footer.clientHeight); //SEM BORDA
+
+// 12 - posicao do elemento
+const product1 = products[0];
+
+console.log(product1.getBoundingClientRect());
+
+// 13 - CSS com JS
+mainContainer.style.color = "red";
+mainContainer.style.backgroundColor = "#333";
+mainContainer.style.paddingBottom = "15px";
+
+// 14 - alterando estilos de varios elementos
+for (const li  of listItens) {
+    li.style.backgroundColor = "red";
+}
