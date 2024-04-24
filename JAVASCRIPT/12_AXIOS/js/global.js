5b4d770fd2b480818de55106c9a57febed7b9b0a
GustavoAlbonico <gustavoalbonicogoncalves@hotmail.com>
2024-04-24 19:15:00 -0300

    mais sobre Axios

---
+++ JAVASCRIPT/12_AXIOS/js/global.js
@@ -0,0 +1,3 @@
+//6 - MUDA AS CONFIGURAÇÕES DO AXIOS
+axios.defaults.headers.common["Accept"] = "application/json";
+axios.defaults.headers.common["Authorization"] = "MEUTOKENDOAPP";
--- JAVASCRIPT/12_AXIOS/js/script.js
+++ JAVASCRIPT/12_AXIOS/js/script.js
@@ -5,7 +5,16 @@ console.log(axios);
 const getData = async() => {
 
     try {
-        const  response = await axios.get("https://jsonplaceholder.typicode.com/users")
+        const  response = await postsFetch.get(
+            "/users",
+        //4 - definindo headers
+            {
+                headers: {
+                    "Content-Type" : "aplication/json",
+                    custom: "header",
+                },
+            }
+        );
         
         console.log(response);
 
@@ -16,3 +25,49 @@ const getData = async() => {
 }
 
 getData();
+
+// 3 - imprimindo dados na tela
+
+const container =  document.querySelector("#user-container");
+
+const printData = async () => {
+    const data = await getData();
+
+    console.log(data);
+
+    data.forEach((user) => {
+        const div = document.createElement("div");
+
+        const nameElement =  document.createElement("h2");
+
+        nameElement.textContent = user.name;
+
+        div.appendChild(nameElement);
+
+        const emailElement = document.createElement("p");
+
+        emailElement.textContent = user.email;
+
+        div.appendChild(emailElement);
+
+        container.appendChild(div);
+    });
+};
+
+printData();
+
+// 5 - post
+const form = document.querySelector("#post-form");
+const titleInput = document.querySelector("#title");
+const bodyInput = document.querySelector("#body");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    postsFetch.post("/posts", {
+        title: titleInput.value,
+        body: bodyInput.value,
+        userId: 1,
+
+    });
+});
