10c63dc255dcd167aa1860f115ce99cb1a26ac6a
Usuario <gustavoalbonicogoncalves@hotmail.com>
2024-07-13 17:27:01 -0300

    Css no react

---
+++ REACTJS/4_CSS_REACT/css_react/src/components/MyComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./MyComponent.css";
+
+const MyComponent = () => {
+  return (
+    <div>
+        <h1>CSS de componente</h1>
+        <h1>Este paragrafo vai ter um estilo</h1>
+    </div>
+  )
+}
+
+export default MyComponent
