cbdeb920d333997ac0a678ef06ec77c8ebd1b07e
Usuario <gustavoalbonicogoncalves@hotmail.com>
2024-07-22 22:12:48 -0300

    Finalizando quiz

--- REACTJS/PROJETOS/3_QUIZ/quiz/src/App.jsx
+++ REACTJS/PROJETOS/3_QUIZ/quiz/src/App.jsx
@@ -4,20 +4,18 @@ import { QuizContext } from "./context/quiz";
 import Welcome from './components/Welcome/Welcome'
 import Question from "./components/Question/Question";
 import GameOver from "./components/GameOver/GameOver";
+import PickCategory from "./components/PickCategory/PickCategory";
 
 import './App.css';
 
 function App() {
 const [quizState, dispatch] = useContext(QuizContext);
 
-useEffect(() => {
-    dispatch({type: "REORDER_QUESTIONS"})
-},[]);
-
   return (
     <div className='App'>
       <h1>Quiz de Programação</h1>
       {quizState.gameStage === "Start" && <Welcome/>}
+      {quizState.gameStage === "Category" && <PickCategory/>}
       {quizState.gameStage === "Playing" && <Question/>}
       {quizState.gameStage === "End" && <GameOver/>}
     </div>
--- REACTJS/PROJETOS/3_QUIZ/quiz/src/context/quiz.jsx
+++ REACTJS/PROJETOS/3_QUIZ/quiz/src/context/quiz.jsx
@@ -1,7 +1,7 @@
-import { act, createContext, useReducer } from "react";
-import questions from '../data/question';
+import { createContext, useReducer } from "react";
+import questions from '../data/question_complete';
 
-const STAGES = ["Start", "Playing", "End"];
+const STAGES = ["Start", "Category", "Playing", "End"];
 
 const inicitalState = {
     gameStage: STAGES[0],
@@ -9,6 +9,8 @@ const inicitalState = {
     currentQuestion: 0,
     score: 0,
     answeSelected: false,
+    help: false,
+    optionToHide: null,
 }
 
 const quizReducer = (state, action) => {
@@ -19,8 +21,22 @@ const quizReducer = (state, action) => {
                 ...state,
                 gameStage: STAGES[1],
             };
+        case "START_GAME":
+            let quizQuestions = null;
+
+            state.questions.forEach((question) => {
+                if(question.category === action.payload){
+                    quizQuestions = question.questions;
+                }
+            })
+
+            return {
+                ...state,
+                questions: quizQuestions,
+                gameStage: STAGES[2]
+            }
         case "REORDER_QUESTIONS":
-            const reorderedQuestions = questions.sort(() => {
+            const reorderedQuestions = state.questions.sort(() => {
                 return Math.random() - 0.5;
             })
 
@@ -32,15 +48,16 @@ const quizReducer = (state, action) => {
             const nextQuestion = state.currentQuestion + 1;
             let endGame = false;
 
-            if(!questions[nextQuestion]){
+            if(!state.questions[nextQuestion]){
                 endGame = true;
             }
 
             return {
                 ...state,
                 currentQuestion: nextQuestion,
-                gameStage: endGame ? STAGES[2] : state.gameStage,
-                answerSelected: false
+                gameStage: endGame ? STAGES[3] : state.gameStage,
+                answerSelected: false,
+                help: false,
             }
         case "NEW_GAME":
             return inicitalState;
@@ -60,8 +77,29 @@ const quizReducer = (state, action) => {
                 score: state.score + correctAnswer,
                 answerSelected: option,
             }
+        case "SHOW_TIP":
+            return{
+                ...state,
+                help: "tip"
+            }
+        case "REMOVE_OPTION":
+            const questionWithoutOption = state.questions[state.currentQuestion];
 
+            let repeat = true;
+            let optionToHide;
 
+            questionWithoutOption.options.forEach((option) => {
+                if(option !== questionWithoutOption.answer && repeat){
+                    optionToHide = option;
+                    repeat = false;
+                }
+            });
+
+            return {
+                ...state,
+                optionToHide,
+                help: true,
+            }
         default:
             return state;
     }
