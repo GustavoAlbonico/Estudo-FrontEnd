a727cd5cd11fcfc2270719e236bc423e7aaf2b3a
gustavo.albonico <gustavoalbonicogoncalves@hotmail.com>
2024-12-26 11:05:16 -0300

    Atualizando caminho para deploy

--- REACTJS/PROJETOS/1_CALCULADORA_IMC/calc-imc/vite.config.js
+++ REACTJS/PROJETOS/1_CALCULADORA_IMC/calc-imc/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/calculadora-imc",
+  base:"/Estudo-FrontEnd/REACTJS/PROJETOS/1_CALCULADORA_IMC/calc-imc",
 })
