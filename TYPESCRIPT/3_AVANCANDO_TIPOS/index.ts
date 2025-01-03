77f51318db6d3026c6d23729197373e036f33b36
gustavo.albonico <gustavoalbonicogoncalves@hotmail.com>
2025-01-03 13:23:02 -0300

    Mais sobre tipos em typescript

--- TYPESCRIPT/3_AVANCANDO_TIPOS/index.ts
+++ TYPESCRIPT/3_AVANCANDO_TIPOS/index.ts
@@ -11,3 +11,82 @@ console.log(nums[2])
 const nomes = ['Matheus', 'Pedro']
 
 // nomes.push(2)
+
+// 2 - outra sintaxe de array
+const numbers: Array<number> = [100, 200]
+
+numbers.push(200)
+
+// numbers.push('teste')
+
+console.log(numbers[1])
+
+// 3 - any
+const arr: any[] = [1, 'teste', true, { nome: 'Matheus' }]
+
+console.log(arr)
+
+arr.push([1, 2, 3])
+
+console.log(arr)
+
+// 4 - parametro de funcao
+
+function soma(a: number, b: number) {
+    console.log(a + b)
+}
+
+soma(4, 5)
+
+// soma('ads', 1)
+
+// 5 - retorno de funcao
+function greeting(name: string): string {
+    return `Olá ${name}!`
+    // return 1
+}
+
+console.log(greeting('Matheus'))
+
+// greeting(1)
+
+// 6 - funcoes anonimas
+setTimeout(function () {
+    const sallary = 1000
+
+    // console.log(parseFloat(sallary))
+}, 5000)
+
+// 7 - tipos de objetos
+
+function passCoordinates(coord: { x: number; y: number }) {
+    console.log('X coordinates: ' + coord.x)
+    console.log('X coordinates: ' + coord.y)
+}
+
+const objCoord = { x: 10, y: 5 }
+
+passCoordinates(objCoord)
+
+// 8 - propriedades opcionais
+function showNumbers(a: number, b: number, c?: number) {
+    console.log('A: ' + a)
+    console.log('B: ' + b)
+    console.log('C: ' + c)
+}
+
+showNumbers(1, 2, 3)
+showNumbers(4, 8)
+// showNumbers(4)
+
+// 9 - validando parâmetro opcional
+function advancedGreeting(firstName: string, lastName?: string) {
+    if (lastName !== undefined) {
+        console.log(`Olá, ${firstName} ${lastName}, tudo bem?`)
+    }
+
+    console.log(`Olá, ${firstName}, tudo bem?`)
+}
+
+advancedGreeting('Matheus', 'Battisti')
+advancedGreeting('João')
